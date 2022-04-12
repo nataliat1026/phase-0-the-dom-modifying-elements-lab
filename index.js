b478a98b6f525836e0986b6f96b1c6858317cf9b
Natalia Teixeira <nataliat1026@gmail.com>
2022-04-12 18:55:24 -0400

    commit tree 83fb655e163e12f0bf8079e89693420b5a5a1669

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainMain = document.querySelector('#main');
+
+mainMain.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'YOUR-NAME is the champion';
